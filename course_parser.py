ae8e2997b7819ffe690ef535f098a843590be84a
Kain <kainedezz.2000@gmail.com>
2020-12-02 01:58:57 +0500

    Добавленна обработка ошибок для response

--- course_parser.py
+++ course_parser.py
@@ -14,12 +14,14 @@ class Currency:
 async def get_table() -> List[Currency]:
     client = ClientSession()
     response = await client.get(CBR_URL)
-    doc = html.document_fromstring(await response.text())
 
     data = []
-    for line in doc.xpath('//tr/td/parent::*'):
-        cells = line.getchildren()
-        data.append(Currency(cells[1].text, float(cells[4].text.replace(',', '.')) / int(cells[2].text)))
+    if response.status == 200:
+        doc = html.document_fromstring(await response.text())
+
+        for line in doc.xpath('//tr/td/parent::*'):
+            cells = line.getchildren()
+            data.append(Currency(cells[1].text, float(cells[4].text.replace(',', '.')) / int(cells[2].text)))
 
     await client.close()
     return data
